77a9db09a91ceed090b5f90909d628f1b0c04eee
Niklas Emevi <niklasm@meridium.se>
2013-03-25 22:01:02 +0100

    init

---
+++ backlight/Scripts/backlight.js
@@ -0,0 +1,40 @@
+﻿
+
+var backlightLoadData = new Object();
+
+$("document").ready(function () {
+
+
+    $("[data-backlight=true]").each(function () {
+        backlightLoadData[$(this).attr("id")] = $(this).html();
+    });
+
+});
+
+window.onbeforeunload = function () {
+
+
+    var itemToSave = {};
+
+    $("[data-backlight=true]").each(function () {
+        var onLoadHtml = backlightLoadData[$(this).attr("id")];
+
+        var nowHtml = $("#" + $(this).attr("id"));
+
+        if (onLoadHtml != nowHtml) {
+            itemToSave[$(this).attr("id")] = nowHtml.html();
+        }
+
+
+    });
+
+    history.pushState(itemToSave, "");
+
+};
+
+window.onpopstate = function (event) {
+
+
+    console.log('popstate fired!');     
+
+};
