15813057fe7b1fabb7dbfce5d7dcb446d7373891
Niklas Emevi <niklasm@meridium.se>
2013-03-26 10:45:56 +0100

    the backlight script now readds content

--- backlight/Scripts/backlight.js
+++ backlight/Scripts/backlight.js
@@ -1,40 +1,49 @@
 ﻿
+Backlight = function () {
+    var backlightLoadData = new Object();
 
-var backlightLoadData = new Object();
+    $("document").ready(function () {
+        $("[data-backlight=true]").each(function () {
+            backlightLoadData[$(this).attr("id")] = $(this).html();
+        });
+        //the browser check is dead in the new jquery
+        if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1 && history.state != null) {            
+            resetState(history.state)
+        };
 
-$("document").ready(function () {
-
-
-    $("[data-backlight=true]").each(function () {
-        backlightLoadData[$(this).attr("id")] = $(this).html();
     });
 
-});
-
-window.onbeforeunload = function () {
-
-
-    var itemToSave = {};
-
-    $("[data-backlight=true]").each(function () {
-        var onLoadHtml = backlightLoadData[$(this).attr("id")];
-
-        var nowHtml = $("#" + $(this).attr("id"));
-
-        if (onLoadHtml != nowHtml) {
-            itemToSave[$(this).attr("id")] = nowHtml.html();
+    var resetState = function (state) {
+        for (var prop in state) {
+            $("#" + prop).html(state[prop]);
         }
 
+    }
 
-    });
+    window.onbeforeunload = function () {
+        var itemToSave = {};
 
-    history.pushState(itemToSave, "");
+        $("[data-backlight=true]").each(function () {
+            var onLoadHtml = backlightLoadData[$(this).attr("id")];
 
-};
+            var nowHtml = $("#" + $(this).attr("id"));
 
-window.onpopstate = function (event) {
+            if (onLoadHtml != nowHtml) {
+                itemToSave[$(this).attr("id")] = nowHtml.html();
+            }
 
 
-    console.log('popstate fired!');     
+        });
 
-};
+        history.pushState(itemToSave, "");
+
+    };
+    window.onpopstate = function (event) {
+        if (event.state == null) {
+            return;
+        }
+        resetState(event.state);
+    };
+
+} ();
+
